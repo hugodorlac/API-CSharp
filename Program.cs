d78c68f2249a62e3d03cba2628a7dd63dc0e17f9
hugo.dorlac <hugo.dorlac@gmail.com>
2023-03-10 11:45:30 +0100

    CORS fonctionne !!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
@@ -7,28 +8,39 @@ using System.Runtime.CompilerServices;
 using XefiAcademyAPI.Model;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args); string MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-    options.MapType<DateOnly>(() => new OpenApiSchema
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    builder =>
     {
-        Type = "string",
-        Format = "date",
-        Example = new OpenApiString("2022-01-01")
-    }));
-
-
-var app = builder.Build();
-
+        builder.WithOrigins("http://172.16.2.98:5500")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod(); 
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); var app = builder.Build(); app.UseCors(MyAllowSpecificOrigins); 
+app.UseCors(builder => builder
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader()
+); 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapGet("/", () =>
+{
+    return "HELLOOOOOOOOOOOOOOOOw";
+})
+.WithName("HelloWord")
+.WithOpenApi();
 
 // -------------------------- CONNAISSANCES -------------------------------------------------------------------------------------------
 
